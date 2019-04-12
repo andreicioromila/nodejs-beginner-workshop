5f7e45e56af8fed3c09a01692fd4f25ddc26dd1f
Vlad Minuti <minutivlad@gmail.com>
2019-04-12 08:44:12 +0300

    Added new http-server example and respective express-core

--- express-core/app.js
+++ express-core/app.js
@@ -2,53 +2,62 @@ const express = require('express');
 
 let app = express();
 
+let id=0
+const jokes = [{id,joke:'A horseman had a horse and the horse had nothing against it',punchLine:''}]
+
+
 app.use((req, res, next) => {
 	console.log(`${req.method}: ${req.url} ${new Date().toString()}`);
 	next();
 });
 
+const findJoke=(req, res, next) => {
+	let i
+	for(i=0;i<jokes.length;i++){
+		if(JSON.stringify(jokes[i].id)===req.params.id){
+			req.location=i;
+			next()
+		}
+	}
+	if(i===jokes.length&&req.params.id!=jokes[i-1].id){
+		res.writeHead(404)
+		res.end()
+	}
+}
+
 app.use(express.static(`${__dirname}/public`));
 
-app.get('/', (req, res) => {
-	res.send('Welcome to our app');
+app.get('/jokes/:id',findJoke, (req, res) => {
+	res.json(jokes[req.location]);
 });
 
-app.post('/posts', (req, res) => {
-	console.log(req.body);
-	let data = '';
-	req.on('data', chunk => {
-		data += chunk;
-	});
+app.get('/jokes', (req, res) => {
+	res.json(jokes);
+});
 
-	req.on('end', () => {
-		let newData = JSON.parse(data);
-		res.json(newData);
-		res.end();
-	});
-	//res.send(req.body.firstName);
+app.post('/jokes', (req, res) => {
+	let joke = {id,joke:req.body.joke,punchLine:req.body.punchLine?req.body.punchLine:''}
+	jokes.push(joke)
+	res.json(joke)
 })
 
-app.get('/posts', (req, res) => {
-	console.log(req, res);
-	res.json([{
-		id: 1,
-		title: 'Welcome'
-	}, {
-		id: 2,
-		title: 'To our blog'
-	}, {
-		id: 3,
-		title: 'Description'
-	}]);
-});
+app.delete('/jokes/:id',findJoke, (req, res) => {
+	jokes.splice(i,1)
+	res.writeHead(200)
+	res.end()
+})
 
-app.get('/posts/:id', (req, res) => {
-	res.send(req.params.id);
-});
+app.put('/jokes/:id',findJoke, (req, res) => {
+	let joke = {id:req.params.id,joke:req.body.joke,punchLine:req.body.punchLine?req.body.punchLine:''}
+	jokes[i]=joke
+	res.json(joke)
+})
 
-app.get('/comments', (req, res) => {
-	res.send(`Sort ${req.query.sort}`);
-});
+app.patch('/jokes/:id',findJoke, (req, res) => {
+	let joke = {id:req.params.id,joke:req.body.joke?req.body.joke:jokes[i].joke,punchLine:req.body.punchLine?req.body.punchLine:jokes[i].punchLine}
+	jokes[i]=joke
+	res.json(joke)
+})
 
 app.listen(8080, () => {
 	console.log('start');
--- http-server/05.server-api-url-parse.js
+++ http-server/05.server-api-url-parse.js
@@ -1,7 +1,8 @@
 const http = require('http')
 const url = require('url')
 const fs = require('fs')
-const jokes = ['A horseman had a horse and the horse had nothing against it']
+let id=0
+const jokes = [{id,joke:'A horseman had a horse and the horse had nothing against it',punchLine:''}]
 
 http.createServer((req, res) => {
   let path = url.parse(req.url).path
@@ -9,7 +10,6 @@ http.createServer((req, res) => {
   if (path === '/') {
     return fs.createReadStream('index.html').pipe(res)
   }
-
   if (path === '/jokes') {
     if (req.method === 'GET') {
 
@@ -22,19 +22,58 @@ http.createServer((req, res) => {
       })
       req.on('end', () => {
         let joke = JSON.parse(postBody)
-        jokes.push(joke)
+        id+=1
+        jokes.push({id,joke:joke.joke,punchLine:joke.punchLine?joke.punchLine:''})// use ...joke
         res.write(JSON.stringify(jokes))
         res.end()
       })
-    } else if (req.method === 'DELETE') {
-      let id = parseInt(path.split('/').pop())
-      if (id < jokes.length) {
-        jokes.splice(id, 1)
-        res.writeHead(200)
-      } else {
-        res.writeHead(404)
+    }
+  }else {
+    let newPath=path.split('/')
+    let reqId=parseInt(newPath[2])
+    let i
+    let id=-99
+    for(i=0;i<jokes.length;i++){
+      if(jokes[i].id===reqId){
+        id=i
+        break
       }
+    }
+    if(id<0){
+      res.writeHead(404)
       res.end()
+    }
+    if(newPath[1]==='jokes'){
+      if(req.method==='GET'){
+        res.write(JSON.stringify(jokes[i]))
+        res.end()
+      } else if(req.method==='DELETE'){
+              jokes.splice(i, 1)
+              res.writeHead(200)
+              res.end()
+        }else if(req.method==='PUT'){
+              let putBody = ''
+              req.on('data', data => {
+                putBody += data
+              })
+              req.on('end', () => {
+                let joke = JSON.parse(putBody)
+                jokes[i]={id:reqId,joke:joke.joke,punchLine:joke.punchLine?joke.punchLine:''}
+                res.write(JSON.stringify(jokes))
+                res.end()
+              })
+        }else if(req.method==='PATCH'){
+              let putBody = ''
+              req.on('data', data => {
+                putBody += data
+              })
+              req.on('end', () => {
+                let joke = JSON.parse(putBody)
+                jokes[i]={id:req.id,joke:joke.joke?joke.joke:jokes[i].joke,punchLine:joke.punchLine?joke.punchLine:jokes[i].punchLine}
+                res.write(JSON.stringify(jokes))
+                res.end()
+              })
+        }
     }
   }
 }).listen(8080, () => {
