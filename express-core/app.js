d511f95a06df80f18da93c8078d2c5cc77213beb
Andrei Cioromila <andrei.cioromila@thinslices.com>
2019-04-12 20:30:15 +0300

    express-core: extract findJokesIndex method and replace middleware

--- express-core/app.js
+++ express-core/app.js
@@ -1,64 +1,83 @@
-const express = require('express');
+const express = require('express')
 
-let app = express();
+let app = express()
 
-let id=0
-const jokes = [{id,joke:'A horseman had a horse and the horse had nothing against it',punchLine:''}]
-
+let id = 0
+const jokes = [{ id, joke: 'A horseman had a horse and the horse had nothing against it', punchLine: '' }]
 
 app.use((req, res, next) => {
-	console.log(`${req.method}: ${req.url} ${new Date().toString()}`);
-	next();
-});
+  console.log(`${req.method}: ${req.url} ${new Date().toString()}`)
+  next()
+})
 
-const findJoke=(req, res, next) => {
-	let i
-	for(i=0;i<jokes.length;i++){
-		if(JSON.stringify(jokes[i].id)===req.params.id){
-			req.location=i;
-			next()
-		}
-	}
-	if(i===jokes.length&&req.params.id!=jokes[i-1].id){
-		res.writeHead(404)
-		res.end()
-	}
+const findJokeIndex = (id) => {
+  id = parseInt(id)
+
+  return jokes.findIndex(joke => joke.id === id)
 }
 
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(`${__dirname}/public`))
 
-app.get('/jokes/:id',findJoke, (req, res) => {
-	res.json(jokes[req.location]);
-});
+app.get('/jokes/:id', (req, res) => {
+  const index = findJokeIndex(req.params.id)
+
+  if (!index) {
+    res.status(404).end()
+  }
+
+  res.json(jokes[index])
+})
 
 app.get('/jokes', (req, res) => {
-	res.json(jokes);
-});
+  res.json(jokes)
+})
 
 app.post('/jokes', (req, res) => {
-	let joke = {id,joke:req.body.joke,punchLine:req.body.punchLine?req.body.punchLine:''}
-	jokes.push(joke)
-	res.json(joke)
+  let joke = { id, joke: req.body.joke, punchLine: req.body.punchLine ? req.body.punchLine : '' }
+  jokes.push(joke)
+  res.json(joke)
 })
 
-app.delete('/jokes/:id',findJoke, (req, res) => {
-	jokes.splice(i,1)
-	res.writeHead(200)
-	res.end()
+app.delete('/jokes/:id', (req, res) => {
+  const index = findJokeIndex(req.params.id)
+
+  if (!index) {
+    res.status(404).end()
+  }
+
+  jokes.splice(index, 1)
+  res.status(200)
+  res.end()
 })
 
-app.put('/jokes/:id',findJoke, (req, res) => {
-	let joke = {id:req.params.id,joke:req.body.joke,punchLine:req.body.punchLine?req.body.punchLine:''}
-	jokes[i]=joke
-	res.json(joke)
+app.put('/jokes/:id', (req, res) => {
+  const index = findJokeIndex(req.params.id)
+
+  if (!index) {
+    res.status(404).end()
+  }
+
+  let joke = { id: req.params.id, joke: req.body.joke, punchLine: req.body.punchLine ? req.body.punchLine : '' }
+  jokes[index] = joke
+  res.json(joke)
 })
 
-app.patch('/jokes/:id',findJoke, (req, res) => {
-	let joke = {id:req.params.id,joke:req.body.joke?req.body.joke:jokes[i].joke,punchLine:req.body.punchLine?req.body.punchLine:jokes[i].punchLine}
-	jokes[i]=joke
-	res.json(joke)
+app.patch('/jokes/:id', (req, res) => {
+  const index = findJokeIndex(req.params.id)
+
+  if (!index) {
+    res.status(404).end()
+  }
+
+  let joke = {
+    id: req.params.id,
+    joke: req.body.joke ? req.body.joke : jokes[i].joke,
+    punchLine: req.body.punchLine ? req.body.punchLine : jokes[i].punchLine
+  }
+  jokes[index] = joke
+  res.json(joke)
 })
 
 app.listen(8080, () => {
-	console.log('start');
-});
+  console.log('start')
+})
